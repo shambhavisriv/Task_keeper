61f93c135c63e154f5eb5ed741490be8a5f684c6
shambhavisriv <shambhavisrivastava010@gmail.com>
2021-05-17 14:17:28 +0530

    made all routing

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,16 @@ const router = express.Router();
 console.log("Router is loaded");
 
 //home page routes
-router.get('/',mainController.homie)
+router.get('/',mainController.homie);
+
+//create task route
+router.post('/create-task',mainController.create);
+
+//route to delete single task
+router.get('/delete-task/:id', mainController.delete);
+
+//route to delete multiple task
+
+router.post('/delete-task',mainController.deleteSelected);
 
 module.exports = router;
