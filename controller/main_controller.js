0152841c309867d00e94f73c35a33538c45b3c81
shambhavisriv <shambhavisrivastava010@gmail.com>
2021-05-17 13:48:51 +0530

    modified controller

--- controller/main_controller.js
+++ controller/main_controller.js
@@ -1,9 +1,9 @@
 //import schema model
-const tasks = require('../models/task');
+const Task = require('../models/task');
 
 //Showing task keeper
 module.exports.homie = (req , res) => {
-    tasks.find({}, (err, tasks) =>{
+    Task.find({}, (err, tasks) =>{
         if(err) { console.log("Error in finding tasks"); return; }
 
         res.render('home', {
@@ -12,4 +12,50 @@ module.exports.homie = (req , res) => {
         });
     });
 };
+//create task
+module.exports.create = (req , res) =>{
+    Task.create({
+        description : req.body.description,
+        dueDate : req.body.date,
+        category : req.body.category
+
+    },(err,task) => {
+        if(err){
+            console.log(`tasks are not created ${err}`);
+            return;
+        }
+        return res.redirect('back');
+
+    } );
+}
+//delete single task
+module.exports.delete = (req,res) =>{
+    Task.findByIdAndDelete(req.params.id ,(err) =>{
+        if(err){
+            console.log(`error in finding id ${err}`);
+            return;
+        }
+        return res.redirect('back');
+        
+    });
+}
+
+//delete multiple tasks
+
+module.exports.deleteSelected = (req ,res) => {
+    Task.deleteMany(
+        {
+            _id:{
+                $in : req.body.task
+            }
+        },
+        (err) => {
+            if(err) {
+                console.log('not found ',err);
+                return;
+            }
+            return res.redirect('back');
+        }
+    );
+}
 
